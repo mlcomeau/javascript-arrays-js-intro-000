69a2b93ca1e76c37460a6bfd52c8a41a5754ed86
Megan Comeau <mlcomeau99@gmail.com>
2020-03-09 00:54:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
 function addElementToBeginningOfArray (array, element) {
   var newArray = [element, ...array];
   return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element)
+  array.unshift(element);
+  return array;
 }
